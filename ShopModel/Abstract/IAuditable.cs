ad4415a6825640a5adda32e23074f550f7b6d1b7
kakalot1519 <54546592+kakalot1519@users.noreply.github.com>
2020-05-17 21:47:36 +0700

    Add domain entities for Project

---
+++ ShopModel/Abstract/IAuditable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Model.Abstract
+{
+    
+   public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        string CreatedBy { set; get; }
+        DateTime? UpdateDate { set; get; }
+        string UpdateBy { set; get; }
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+        bool Status { set; get; }
+    }
+}
--- ShopModel/Properties/AssemblyInfo.cs
+++ ShopModel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ShopModel")]
+[assembly: AssemblyTitle("Shop.Model")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ShopModel")]
+[assembly: AssemblyProduct("Shop.Model")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
